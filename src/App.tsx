5938cf1225400dddf1eb50fb6efc6bf4227f3d8a
vassilisyakamozis-lgtm <vassilis.yakamozis@emea.media>
2025-09-23 15:45:22 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// src/App.tsx
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "@/pages/Home";
@@ -5,61 +6,22 @@ import Cup from "@/pages/Cup";
 import MyReadings from "@/pages/MyReadings";
 import AuthCallback from "@/pages/auth/callback";
 import ReadingStartPage from "@/pages/reading/Start";
-import ReadingDetail from "@/pages/reading/Detail";
+import CupReadingResult from "@/pages/CupReadingResult"; // <-- Χρησιμοποιούμε αυτό
 import AuthRedirectGuard from "@/hooks/AuthRedirectGuard";
-
-import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-      {/* Παραμένει όπως το έχεις */}
       <AuthRedirectGuard />
-
       <Routes>
         <Route path="/" element={<Home />} />
-
-        {/* Προστατευμένες διαδρομές */}
-        <Route
-          path="/cup"
-          element={
-            <ProtectedRoute>
-              <Cup />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/my-readings"
-          element={
-            <ProtectedRoute>
-              <MyReadings />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/reading/start"
-          element={
-            <ProtectedRoute>
-              <ReadingStartPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/reading/:id"
-          element={
-            <ProtectedRoute>
-              <ReadingDetail />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* OAuth callback του Supabase/Google */}
+        <Route path="/cup" element={<Cup />} />
+        <Route path="/my-readings" element={<MyReadings />} />
+        <Route path="/reading/start" element={<ReadingStartPage />} />
+        <Route path="/reading/:id" element={<CupReadingResult />} /> {/* <-- εδώ */}
         <Route path="/auth/callback" element={<AuthCallback />} />
-
-        {/* catch-all */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
